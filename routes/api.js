f1dba9b0b70ae1fad6d2c3aa0e251e91baf53f4a
DuongQuangManh <manhdqph20768@fpt.edu.vn>
2023-06-22 10:53:08 +0700

    update edit profile

--- routes/api.js
+++ routes/api.js
@@ -1,40 +1,42 @@
-var express = require('express')
-var router = express.Router()
-var productApi = require('../controllers/api/product.api')
-var userApi = require('../controllers/api/user.api')
-var orderApi = require('../controllers/api/order.api')
+var express = require("express");
+var router = express.Router();
+var productApi = require("../controllers/api/product.api");
+var userApi = require("../controllers/api/user.api");
+var orderApi = require("../controllers/api/order.api");
 
 // lấy danh sách sản phẩm
-router.get('/products', productApi.getProducts)
+router.get("/products", productApi.getProducts);
 // update sản phẩm với id
-router.post('/products/:id', productApi.updateProduct)
+router.post("/products/:id", productApi.updateProduct);
 
 // lấy danh sách sản phẩm yêu thích với userId
-router.post('/products/favorite/:userId', productApi.getFavoriteProduct)
+router.post("/products/favorite/:userId", productApi.getFavoriteProduct);
 
 // lấy sản phẩm phổ biến
-router.get('/products/populate', productApi.getPopulateProduct)
+router.get("/products/populate", productApi.getPopulateProduct);
 
 //lấy danh sách thể loại
-router.get('/categories', productApi.getCategories)
+router.get("/categories", productApi.getCategories);
 
 // lấy user với id
-router.get('/users/:userId', productApi.getUserWithId)
-
+router.get("/users/:userId", productApi.getUserWithId);
+router.post("/users/update/:id", userApi.edit);
 //lấy hóa đơn với userId
-router.post('/orders/get/:userId', productApi.getOrderWithUser)
+router.post("/orders/get/:userId", productApi.getOrderWithUser);
 //thanh toán hóa đơn
-router.post('/orders/:userId', productApi.postOrder)
+router.post("/orders/:userId", productApi.postOrder);
 
 // láy giỏ hàng yêu thích
-router.post('/orders/favorite/:userId', orderApi.getFavoriteOrder)
-router.post('/orders/favorite/add/:userId', orderApi.addFavoriteOrder)
-router.post('/orders/favorite/delete/:userId', orderApi.deleteOrderFavorite)
-router.post('/orders/favorite/addProduct/:userId', orderApi.addProductToOrderFavorite)
-router.post('/orders/favorite/update/:userId', orderApi.updateFavoriteOrder)
+router.post("/orders/favorite/:userId", orderApi.getFavoriteOrder);
+router.post("/orders/favorite/add/:userId", orderApi.addFavoriteOrder);
+router.post("/orders/favorite/delete/:userId", orderApi.deleteOrderFavorite);
+router.post(
+  "/orders/favorite/addProduct/:userId",
+  orderApi.addProductToOrderFavorite
+);
+router.post("/orders/favorite/update/:userId", orderApi.updateFavoriteOrder);
 
+router.post("/login", userApi.login);
+router.post("/register", userApi.register);
 
-router.post('/login', userApi.login)
-router.post('/register', userApi.register)
-
-module.exports = router
+module.exports = router;
